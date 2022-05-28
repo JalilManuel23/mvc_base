923ee8436e0c43e308f2b103738d60001a6a952c
Jalil <jalilmanuel25@gmail.com>
2022-05-28 12:58:56 -0500

    correciones

--- libs/app.php
+++ libs/app.php
@@ -27,12 +27,22 @@ class App {
             $controller = new $url[0];
             $controller->loadModel($url[0]);
 
-            // si hay un método que se requiere cargar
-            if(isset($url[1])) {
-                $controller->{$url[1]}();
-            } else {
-                $controller->render();
-            }
+           // # elementos del arreglo
+           $nparam = sizeof($url);
+
+           if($nparam > 1){
+               if($nparam > 2){
+                   $param = [];
+                   for($i = 2; $i<$nparam; $i++){
+                       array_push($param, $url[$i]);
+                   }
+                   $controller->{$url[1]}($param);
+               }else{
+                   $controller->{$url[1]}();
+               }
+           }else{
+               $controller->render();
+           }
         } else {
             $controller = new Errores();
         }
